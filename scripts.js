0bcdb91004e1f7f11f93f1f15e1e019d3c5138dc
annmcalvert <annmcalvert@gmail.com>
2018-10-09 17:26:06 -0500

    progress step 11

--- scripts.js
+++ scripts.js
@@ -1,9 +1,49 @@
-let div = document.createElement('div.header-container');
-let header = document.createElement('h1');
-let headerText = document.createTextNode('This is an h1');
 
-header.appendChild(headerText);
-div.appendChild(header);
+
+let div = document.createElement('div');
+div.setAttribute("class", "header-container");
+let header1 = document.createElement('h1');
+header1.setAttribute("class", "h1");
+let headerTextH1 = document.createTextNode('This is an h1');
+let header2 = document.createElement('h2');
+header2.setAttribute("class", "h2");
+let headerTextH2 = document.createTextNode('This is an h2');
+let header3 = document.createElement('h3');
+header3.setAttribute("class", "h3");
+let headerTextH3 = document.createTextNode('This is an h3');
+let header4 = document.createElement('h4');
+header4.setAttribute("class", "h4");
+let headerTextH4 = document.createTextNode('This is an h4');
+let header5 = document.createElement('h5');
+header5.setAttribute("class", "h5");
+let headerTextH5 = document.createTextNode('This is an h5');
+let header6 = document.createElement('h6');
+header6.setAttribute("class", "h6");
+let headerTextH6 = document.createTextNode('This is an h6');
+
+header1.appendChild(headerTextH1);
+div.appendChild(header1);
+header2.appendChild(headerTextH2);
+div.appendChild(header2);
+header3.appendChild(headerTextH3);
+div.appendChild(header3);
+header4.appendChild(headerTextH4);
+div.appendChild(header4);
+header5.appendChild(headerTextH5);
+div.appendChild(header5);
+header6.appendChild(headerTextH6);
+div.appendChild(header6);
+
+//seems to be working but returns "undefined" sometimes
+let changeColor = ["blue", "green", "yellow", "orange", "red", "purple", "black", "grey"];
+let randomColor = changeColor[Math.ceil(Math.random() *changeColor.length)];
+console.log(randomColor);
+
+//changes color when header1 is clicked
+header1.addEventListener('dblclick', function() {
+    div.getElementsByClassName("h1")[0].style.color = randomColor;
+})
+
 
 window.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(div);
